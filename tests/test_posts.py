1e144801d44f38af284353b6c7277534dbe573d4
Antonio Vilchez <antoniovmonge@gmail.com>
2023-09-30 18:07:17 +0200

    [add] test create comment

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -12,10 +12,25 @@ async def create_post(body: str, async_client: AsyncClient) -> dict:
     return response.json()
 
 
+async def create_comment(body: str, post_id: int, async_client: AsyncClient) -> dict:
+    """Helper function to create a comment"""
+    response = await async_client.post(
+        "/api/v1/posts/comments/",
+        content=json.dumps({"body": body, "post_id": post_id}),
+    )
+    return response.json()
+
+
 @pytest.fixture()
 async def created_post(async_client: AsyncClient) -> dict:
-    """Fixture to create a post"""
+    """Fixture for a created post"""
     return await create_post("Test Post", async_client)
+
+
+@pytest.fixture()
+async def created_comment(async_client: AsyncClient, created_post: dict) -> dict:
+    """Fixture for a created comment"""
+    return await create_post("Test Comment", created_post["id"], async_client)
 
 
 @pytest.mark.anyio
@@ -47,3 +62,20 @@ async def test_read_all_posts(async_client: AsyncClient, created_post: dict):
 
     assert response.status_code == 200
     assert response.json() == [created_post]
+
+
+@pytest.mark.anyio
+async def test_create_comment(async_client: AsyncClient, created_post: dict):
+    body = "Test Comment"
+
+    response = await async_client.post(
+        "/api/v1/posts/comments/",
+        content=json.dumps({"body": body, "post_id": created_post["id"]}),
+    )
+
+    assert response.status_code == 201
+    assert {
+        "id": 1,
+        "body": body,
+        "post_id": created_post["id"],
+    }.items() <= response.json().items()
